cbbbac2b9b4d7a83fa912508c1754275aeecd5a2
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2024-01-09 23:34:05 +0100

    Menu inicial

--- src/javapracticaalumno/JavaPracticaAlumno.java
+++ src/javapracticaalumno/JavaPracticaAlumno.java
@@ -16,7 +16,6 @@ public class JavaPracticaAlumno {
     /**
      * @param args the command line arguments
      */
-
     public static void main(String[] args) {
         // TODO code application logic here
         boolean salir = false;
