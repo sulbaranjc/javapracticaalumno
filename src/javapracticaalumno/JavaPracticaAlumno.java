a1a3cc0597859fdf753135dcd5c9e4c63a9469d8
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2024-01-09 23:35:15 +0100

    Menu inicial

--- src/javapracticaalumno/JavaPracticaAlumno.java
+++ src/javapracticaalumno/JavaPracticaAlumno.java
@@ -1,21 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javapracticaalumno;
 
 import java.util.Scanner;
 
 /**
- *
  * @author sulbaranjc
  */
 public class JavaPracticaAlumno {
+
     public static Scanner scanner = new Scanner(System.in);
 
-
+    /**
+     * Este es el método principal de la aplicación.
+     * @param args los argumentos de línea de comando
+     */
     public static void main(String[] args) {
-        // TODO code application logic here
         boolean salir = false;
         while (!salir) {
             System.out.println("\n===== MENÚ PRINCIPAL =====");
