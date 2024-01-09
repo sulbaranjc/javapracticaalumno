4d4355bcd362a79ec13e21561433acbbab28b5a3
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2024-01-09 23:25:21 +0100

    Menu inicial

--- src/javapracticaalumno/JavaPracticaAlumno.java
+++ src/javapracticaalumno/JavaPracticaAlumno.java
@@ -4,6 +4,8 @@
  */
 package javapracticaalumno;
 
+import java.util.Scanner;
+
 /**
  *
  * @author sulbaranjc
@@ -13,9 +15,46 @@ public class JavaPracticaAlumno {
     /**
      * @param args the command line arguments
      */
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo JC");
+        boolean salir = false;
+        while (!salir) {
+            System.out.println("\n===== MENÚ PRINCIPAL =====");
+            System.out.println("1. Crear Alumno");
+            System.out.println("2. Listar Alumnos");
+            System.out.println("3. Actualizar Alumno");
+            System.out.println("4. Eliminar Alumno");
+            System.out.println("5. Cargar Datos de Ejemplo");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            int opcion = Integer.parseInt(scanner.nextLine());
+
+            switch (opcion) {
+                case 1:
+                    // Código para crear un alumno
+                    break;
+                case 2:
+                    // Código para ver los alumnos
+                    break;
+                case 3:
+                    // Código para actualizar un alumno
+                    break;
+                case 4:
+                    // Código para eliminar un alumno
+                    break;
+                case 5:
+                    break;
+                case 0:
+                    System.out.println("Saliendo del programa...");
+                    System.out.println("Gracias por usar el programa.");
+                    salir = true;
+                default:
+                    System.out.println("Opción no válida. Por favor, intente de nuevo.");
+            }
+        }
     }
-    
+
 }
+    
