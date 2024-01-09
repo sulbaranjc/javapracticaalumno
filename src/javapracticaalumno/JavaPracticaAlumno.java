2c3aefdb84636b328dbf9fb29b2312aec38443f4
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2024-01-09 23:11:27 +0100

    ok

--- src/javapracticaalumno/JavaPracticaAlumno.java
+++ src/javapracticaalumno/JavaPracticaAlumno.java
@@ -15,7 +15,7 @@ public class JavaPracticaAlumno {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo JC");
     }
     
 }
